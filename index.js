f57ad2c4ab60ac3537520a92205936fb831b4bdf
susendra-coder <susendrasusendra62@gmail.com>
2026-03-12 14:40:43 +0530

    update

--- index.js
+++ index.js
@@ -6,28 +6,41 @@ import mongoose from "mongoose";
 import dbConnect from "./config/db.js";
 import productRouter from "./routes/productRoute.js";
 import { storeRouter } from "./routes/storeRoute.js";
+
 const app = express();
 dotenv.config();
+
 app.use(expressLayouts);
+
 app.set("view engine", "ejs");
 app.set("views", "views");
-app.set("layout","layout")
+app.set("layout", "layout");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+/* SESSION */
 app.use(
   session({
     secret: "secretkey",
     resave: false,
     saveUninitialized: false,
-  }),
+  })
 );
 
+/* GLOBAL USER VARIABLE FOR EJS */
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  next();
+});
+
+/* ROUTES */
 app.use("/", storeRouter);
 // app.use("/auth", authRouter);
 app.use("/products", productRouter);
 // app.use("/users", userRouter);
 
+/* START SERVER */
 const startServer = async () => {
   await dbConnect();
   app.listen(5000, () => {
@@ -35,4 +48,4 @@ const startServer = async () => {
   });
 };
 
-startServer()
+startServer();
